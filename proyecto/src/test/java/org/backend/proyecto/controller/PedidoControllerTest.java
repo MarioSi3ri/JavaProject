768c7caff37b74f277f6c6226bf066e9cce25f61
diefpt <diefpt@gmail.com>
2024-02-07 17:06:46 -0600

    Pedido controller tests

--- proyecto/src/test/java/org/backend/proyecto/controller/PedidoControllerTest.java
+++ proyecto/src/test/java/org/backend/proyecto/controller/PedidoControllerTest.java
@@ -1,54 +1,90 @@
 package org.backend.proyecto.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.backend.proyecto.exception.PedidoNotFoundException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import org.backend.proyecto.controller.PedidoController;
+
 import org.backend.proyecto.dto.PedidoDTO;
 import org.backend.proyecto.service.PedidoService;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class PedidoControllerTest {
 
-    @Mock
-    private PedidoService pedidoService;
+    @MockBean
+    private PedidoService service;
 
-    @InjectMocks
-    private PedidoController pedidoController;
+    @Autowired
+    private PedidoController controller;
 
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(pedidoController).build();
+    @Test
+    @DisplayName("Controller should be injected")
+    void smokeTest() {
+        assertNotNull(controller);
     }
 
-    @SuppressWarnings("null")
     @Test
-    void testCreatePedido() throws Exception {
-        // Configuración del mock.
-        PedidoDTO pedidoDTO = new PedidoDTO(1L, true, 0.0);
-        when(pedidoService.createPedido()).thenReturn(pedidoDTO);
+    @DisplayName("Controller should save an order")
+    void createPedidoTest() throws Exception {
+        PedidoDTO dto = new PedidoDTO(1L, true, 0.0);
 
-        // Solicitud HTTP POST al endpoint '/pedidos'.
-        mockMvc.perform(post("/pedidos")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.activo").value(true))
-                .andExpect(jsonPath("$.total").value(0.0));
+        when(service.createPedido()).thenReturn(dto);
+
+        PedidoDTO result = controller.createPedido();
+
+        assertNotNull(result);
+        assertEquals(dto.getId(), result.getId());
+        assertTrue(result.isActivo());
+        assertEquals(0.0, result.getTotal());
+    }
+
+    @Test
+    @DisplayName("Controller should return a list of orders")
+    void getPedidosTest() {
+        PedidoDTO dto1 = new PedidoDTO(1L, true, 0.0);
+        PedidoDTO dto2 = new PedidoDTO(2L, true, 0.0);
+
+        when(service.getAll()).thenReturn(Arrays.asList(dto1, dto2));
+
+        List<PedidoDTO> result = controller.getPedidos();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(dto1.getId(), result.get(0).getId());
+        assertEquals(dto2.getId(), result.get(1).getId());
+    }
+
+    @Test
+    @DisplayName("Controller should return a pedido")
+    void getPedidoTest() throws PedidoNotFoundException {
+        PedidoDTO dto = new PedidoDTO(1L, true, 0.0);
+
+        when(service.getPedido(anyLong())).thenReturn(dto);
+
+        PedidoDTO result = controller.getPedido(1L);
+        assertEquals(dto.getId(), result.getId());
+        assertTrue(result.isActivo());
+        assertEquals(0.0, result.getTotal());
+    }
+
+    @Test
+    @DisplayName("Controller should throw an error if pedido was not found")
+    void getPedidoWithErrorTest() throws PedidoNotFoundException {
+        when(service.getPedido(anyLong())).thenThrow(PedidoNotFoundException.class);
+
+        assertThrows(PedidoNotFoundException.class, () -> controller.getPedido(1L));
     }
 }
