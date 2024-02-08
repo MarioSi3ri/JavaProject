e995fa6a2e596f3e4e4cf228f2a141d77af8f2fe
diefpt <diefpt@gmail.com>
2024-02-07 19:26:15 -0600

    cambio de carpeta para ci / cd

---
+++ src/main/java/org/backend/proyecto/config/OpenAPIConfig.java
@@ -0,0 +1,15 @@
+package org.backend.proyecto.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API de Sistema de Gestión de Productos.",
+                description = "API de nuestro proyecto Sistema de Gestión de Productos.",
+                version = "v0.13.4"
+        )
+)
+public class OpenAPIConfig {
+
+}
